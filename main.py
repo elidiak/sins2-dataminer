322da0443e9816e88db57405dcb5fb11b14de1d6
Eric Lidiak <el8472@gmail.com>
2026-01-30 20:36:26 -0700

    Update main.py
    Adding entry point logic

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from pathlib import Path
 from .cli import parse_args
 from .scanner import scan_install_dir
 from .writers.json_writer import write_json
 
 def main():
+    # Parse command-line arguments
     args = parse_args()
 
+    steamdir = Path(args.steamdir)
+    outdir = Path(args.output)
+    
+    # If the directories do not exist, raise an error
+    if not steamdir.exists():
+        raise FileNotFoundError(f"Steam directory '{steamdir}' does not exist.")
+    
     data = scan_install_dir(args.steamdir)
 
     write_json(data, args.output)
